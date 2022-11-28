9b5962f62761054c5d4e090bbf76557be17715be
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-11-28 21:13:48 +0300

    package version improvements

--- scripts/test.R
+++ scripts/test.R
@@ -35,7 +35,7 @@ files <- list.files(system.file("external", package = "sdm"),
 
 b <- list()
 
-# Simple form of for loop
+# Simple form of afor loop
 
 for (i in 1:length(files)) {
   b[[i]] <- terra::rast(files[[i]])
