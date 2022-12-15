57badc08502454866d5be79f334c4ef040239a68
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-12-15 19:28:39 +0300

    Working with Spark in R

--- scripts/sparklyr.R
+++ scripts/sparklyr.R
@@ -2,7 +2,7 @@
 
 # Task: Working with SPark in R\
 # Date created: 15th December 2022
-# Last modified on 
+# Last modified on 15th 
 
 # devtools::install_github("sparklyr/sparklyr")
 library(sparklyr)
